376738c8f0254b2ef74a43b3473743b8fe4f1b32
hannahlb123 <60858561+hannahlb123@users.noreply.github.com>
2021-08-18 21:46:33 +0000

    code is now basic but functional. Have tested all questions and at this stage they are all working. Added code so that people can't check a question twice. Next steps: add points and reset button. Also add styling.

--- src/App.svelte
+++ src/App.svelte
@@ -5,65 +5,109 @@
 	
 	let questions = [
 				{
-					question:'Does current split or stay constant through a series circuit?',
-					option1:'Same',
-					option2:'Splits',
-					answer:'Same',
+					question:'Does current split or stay constant through a series 			circuit?',
+					option1:'same',
+					option2:'splits',
+					answer:'same',
 					result:'',
+					graphic:'',
 					choice:''
 				},
 				{
 					question:'Does voltage stay constant or split in a series circuit?',
-					option1:'Same',
-					option2:'Splits',
-					answer:'Splits',
+					option1:'same',
+					option2:'splits',
+					answer:'splits',
 					result:'',
+					graphic:'',
+					choice:''
+				},
+        {
+					question:'What is the voltage across the lightbulb in this circuit?',
+					option1:'4V',
+					option2:'6V',
+					answer:'4V',
+					result:'',
+					graphic:'../src/assets/circuit1.png',
 					choice:''
 				}
+			
 	]
-	let i = 1
+	
+	let i = 0
 	let question = questions.at[i]
+  let answering = true
 	
 	function check (response) {
 		if (response == questions[i].answer) {
 			result = 'correct'
 		} else {
 			result = 'wrong'
+      result += ' the correct answer was ' + questions[i].answer
 		}
-		console.log('function is done. result is ' + result)
-		result += ' the correct answer was ' + questions[i].answer
-		return result
+    answering = false
+		console.log('function is done. result is ' + result + 'answering is ' + answering)
+		return result, answering
 	}
+	
+	function next () {
+		console.log('function next running')
+		i += 1
+    answering = true
+		console.log('i = ' + i)
+    result = ''
+    return result, answering
+	}
+
+	function state(choice) {
+		console.log('function state running. answering = ' + answering)
+		if (answering === false) {
+				next ()
+			} else if (answering === true) {
+				check(choice)
+			}
+	}
+	
 	
 </script>
 
 <h1>Electricty! </h1>
 
-<p>
-	hello there. Please bare with the code is not done yet.
-</p>
+{#if (i < questions.length)}
+    {questions[i].question}
 
-<!-- {#if (i < questions.length)} -->
-  {questions[i].question}
-      <label>
-        <input type="radio" bind:group={questions[i].choice} value={questions[i].option1}>
-        {questions[i].option1}
-      </label>
-      <br>
-      <label>
-        <input type="radio" bind:group={questions[i].choice} value={questions[i].option2}>
-        {questions[i].option2}
-      </label>
+  {#if (questions[i].graphic != '')}
+    <img src={questions[i].graphic} alt='not working'>
+  {/if}
+        <br>
+        <label>
+          <input type="radio" bind:group={questions[i].choice} value={questions[i].option1}>
+          {questions[i].option1}
+        </label>
+        <br>
+        <label>
+          <input type="radio" bind:group={questions[i].choice} value={questions[i].option2}>
+          {questions[i].option2}
+        </label>
+        <br>
+        <br>
 
-<label>
-<button on:click={check(questions[i].choice)}>
-	Check answer
-	</button>
-</label>
-
+    {#if answering}
+    <button id='btnCheck' on:click={state(questions[i].choice)}>
+      Check
+    </button>
+    {:else}
+    <button id='btnNext' on:click={state}>
+      Next
+    </button>
+    {/if}
+{:else}
+  <p> You are done :) </p>
+{/if}
 <p>
 	{result}
 </p>
+
 
 <style>
   :root {
@@ -71,19 +115,13 @@
       Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   }
 
-  main {
-    text-align: center;
-    padding: 1em;
-    margin: 0 auto;
-  }
-
   img {
-    height: 16rem;
-    width: 16rem;
+    height: 12rem;
+    width: auto;
   }
 
   h1 {
-    color: #ff3e00;
+    color: #6633FF;
     text-transform: uppercase;
     font-size: 4rem;
     font-weight: 100;
